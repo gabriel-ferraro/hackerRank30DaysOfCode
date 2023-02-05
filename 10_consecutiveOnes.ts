aa1c12e0acf9b1f7659285f53bea0335812cc984
Gabriel342 <gabrielsev_2002@hotmail.com>
2023-02-05 15:56:27 -0300

    day 10 - counting consecutive one's in a binary number

---
+++ 10_consecutiveOnes.ts
@@ -0,0 +1,25 @@
+//create a function that recieves a base 10 integer, this integer must be converted to binary (base 2);
+//the function must return the greatest quantity of consecutive ones present in the binary number, as a base 10 integer;
+
+function consecutiveOnes(n: number) {
+    let currentConsecutiveOnes = 0;
+    let maxConsecutiveOnes = 0;
+
+    while (n > 0) {
+        let remainder = n % 2;
+        if (remainder == 1) {
+            currentConsecutiveOnes += 1;
+            if (currentConsecutiveOnes > maxConsecutiveOnes) {
+                maxConsecutiveOnes = currentConsecutiveOnes;
+            }
+        } else {
+            currentConsecutiveOnes = 0;
+        }
+        n = Math.floor(n / 2);
+    }
+    return maxConsecutiveOnes;
+}
+
+console.log(consecutiveOnes(5));
+console.log(consecutiveOnes(13));
+console.log(consecutiveOnes(30));
