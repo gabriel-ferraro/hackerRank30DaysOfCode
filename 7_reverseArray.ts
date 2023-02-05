8fffffa9ff6e4ac4e9a314571c511f0c3ac94e23
Gabriel342 <gabrielsev_2002@hotmail.com>
2023-02-05 15:43:42 -0300

    day 7 - reverse array and print as a string of elements separated by spaces

---
+++ 7_reverseArray.ts
@@ -0,0 +1,26 @@
+//recieving a number array, reverse it and print it as a string separated by spaces; example:
+
+//numberArray = [4, 3, 2, 1]
+//reversedNumberArray = [1, 2, 3, 4];
+//print the string: "1 2 3 4";
+
+function reverseArray(arr: number[]) {
+    for (let i = 0; i < arr.length / 2; i++) {
+        let swap = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = swap;
+    }
+    return arr;
+}
+
+function addSpaces(str: string) {
+    return str.split(',').join(' ');
+}
+
+function main() {
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    console.log(addSpaces(reverseArray(arr).toString()));
+}
