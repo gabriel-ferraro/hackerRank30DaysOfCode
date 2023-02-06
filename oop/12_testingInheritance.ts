5f7888812cc11248aa74d58621d5b55c34ba1cc4
Gabriel342 <gabrielsev_2002@hotmail.com>
2023-02-06 14:27:46 -0300

    corrected exercises

---
+++ oop/12_testingInheritance.ts
@@ -0,0 +1,42 @@
+
+class Person {
+    firstName: string;
+    lastName: string;
+    idNumber: number;
+
+    constructor(firstName: string, lastName: string, idNumber: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+    }
+
+    printPerson() {
+        console.log("Name: " + this.lastName + ", " + this.firstName + "\nID: " + this.idNumber);
+    }
+}
+
+class Student extends Person {
+    idNumber: number;
+    scores: number[];
+
+    constructor(firstName: string, lastName: string, idNumber: number, scores: number[]) {
+        super(firstName, lastName, idNumber);
+        this.scores = scores;
+    }
+
+    getLetterGrade(grade: number): string {
+        if (grade < 40) return "T";
+        else if (grade >= 40 && grade < 55) return "D";
+        else if (grade >= 55 && grade < 70) return "P";
+        else if (grade >= 70 && grade < 80) return "A";
+        else if (grade >= 80 && grade < 90) return "E";
+        else return "O";
+    }
+
+    calculate() {
+        const sum = this.scores.reduce((a, b) => a += b);
+        const score = sum / this.scores.length;
+
+        return this.getLetterGrade(score);
+    }
+}
